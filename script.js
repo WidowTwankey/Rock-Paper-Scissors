4e7544740f492339b538f17a7b736c99301a1784
WidowTwankey <phprotheroe@gmail.com>
2023-01-05 19:56:14 +0000

    added pseudocode and prompt for user

---
+++ script.js
@@ -0,0 +1,33 @@
+- prompt user to choose rock, paper or scissors
+let userInput = prompt("Choose Rock, Paper or Scissors")
+switch (userInput){
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3: 
+        break;
+    }
+
+
+
+
+
+
+- random generator rock, paper or scissors
+
+- switch case for each scenario - console log
+user rock com rock - draw
+user rock com scissors - you win!
+user rock com paper - you lose
+user paper com paper - draw
+user paper com scissors - you lose
+user paper com rock - you win!
+user scissors com scissors - draw
+user scissors com paper - you win!
+user scissors com rock - you lose
+
+- create total score array of wins, losses and draws
+- add +1 to their wins, losses or draws variable
+
+for loop whole thing until total score array = 10
